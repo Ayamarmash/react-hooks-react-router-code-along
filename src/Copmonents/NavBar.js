9a3370a574f43e0d4154ea46f6d2430eb37cd41c
ayamarmash <ayamarmash74@gmail.com>
2023-01-02 10:42:23 +0200

    Done

---
+++ src/Copmonents/NavBar.js
@@ -0,0 +1,63 @@
+/* Add basic styling for NavLinks */
+import { NavLink } from "react-router-dom";
+
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+function NavBar() {
+    return (
+      <div>
+        <NavLink
+          to="/"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={linkStyles}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Login
+        </NavLink>
+        <NavLink
+        to="/SignUp"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+        >
+          SignUp
+        </NavLink>
+      </div>
+    );
+  }
+  export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+/* Add NavLink to import */
+import { BrowserRouter, Route , Switch } from "react-router-dom";
+import Home from "./Copmonents/Home";
+import About from "./Copmonents/About";
+import Login from "./Copmonents/Login";
+import SignUp from "./Copmonents/SignUp";
+import NavBar from "./Copmonents/NavBar";
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signUp">
+        <SignUp/>
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
